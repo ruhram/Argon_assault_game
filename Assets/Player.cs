dd3092932ba69cec241811c945b6b631eae61245
Ruhram <rullyrgandadiwirya@gmail.com>
2021-02-23 15:53:12 +0700

    y position clamped

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,7 +5,9 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     [Tooltip("In ms^-1")][SerializeField] float xSpeed = 20f;
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 20f;
     [Tooltip("In m")] [SerializeField] float xRange = 5f;
+    [Tooltip("In m")] [SerializeField] float yRange = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +18,15 @@ public class Player : MonoBehaviour
     void Update()
     {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
         float xOffset = xThrow * xSpeed * Time.deltaTime;
         float rawXPos = transform.localPosition.x + xOffset;
+        float rawYPos = transform.localPosition.y + yOffset;
         float clampedXpos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        float clampedYpos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
-        transform.localPosition = new Vector3(clampedXpos, transform.localPosition.y, transform.localPosition.z);
+        transform.localPosition = new Vector3(clampedXpos, clampedYpos, transform.localPosition.z);
 
     }
 }
