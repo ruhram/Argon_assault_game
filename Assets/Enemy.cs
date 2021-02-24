c15048c992ccafe726ea34ac351a9a7e3c320e61
Ruhram <rullyrgandadiwirya@gmail.com>
2021-02-24 14:56:36 +0700

    Calling The Component of Collision

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +8,13 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        addNonTriggerCollider();
+    }
+
+    private void addNonTriggerCollider()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
     }
 
     // Update is called once per frame
