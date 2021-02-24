b51844432fd63a41436e69f610a42a5ccd0f923d
Ruhram <rullyrgandadiwirya@gmail.com>
2021-02-24 15:35:03 +0700

    Instantiate gameObject

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +27,8 @@ public class Enemy : MonoBehaviour
 
     private void OnParticleCollision(GameObject other)
     {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
